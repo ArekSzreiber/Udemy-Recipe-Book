6feb8c751601442b1ec13aedca8be2c7048e43a3
Arkadiusz Szreiber <szreiber.arkadiusz@gmail.com>
2019-12-20 15:08:39 +0100

    57. Creating and Outputting the Shopping List

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,8 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients: [];
-  constructor() { }
+  ingredients: Ingredient[] = [
+    new Ingredient('Eggs', 4),
+    new Ingredient('Tomatoes', 2),
+    new Ingredient('Potatoes', 7),
+    new Ingredient('Salt', 1),
+    new Ingredient('Pepper', 1),
+    new Ingredient('Onions', 5)
+  ];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
