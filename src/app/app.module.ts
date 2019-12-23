3eab9ef44f07fcaca316b6bd44026253e20d3920
Arkadiusz Szreiber <szreiber.arkadiusz@gmail.com>
2019-12-23 20:51:28 +0100

    102. Building and Using a Dropdown Directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,12 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import {FormsModule} from '@angular/forms';
+import {DropdownDirective} from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
+    DropdownDirective,
     HeaderComponent,
     RecipesComponent,
     RecipeListComponent,
