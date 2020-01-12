5711070cb20a1d677d6ad62746a804a7de4481fc
Arkadiusz Szreiber <szreiber.arkadiusz@gmail.com>
2020-01-12 01:09:17 +0100

    281. Fetching Recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {RecipeService} from '../recipes/recipe.service';
+import {Recipe} from '../recipes/recipe.model';
 
 @Injectable({
   providedIn: 'root'
@@ -19,6 +20,17 @@ export class DataStorageService {
       )
       .subscribe(response => {
         console.log(response);
+      });
+  }
+
+  fetchRecipes() {
+    this.http
+      .get<Recipe[]>(
+        'https://udemy-angular-project-1be0a.firebaseio.com/recipes.json'
+      )
+      .subscribe(recipes => {
+        // console.log(recipes);
+        this.recipeService.setRecipes(recipes);
       });
   }
 }
