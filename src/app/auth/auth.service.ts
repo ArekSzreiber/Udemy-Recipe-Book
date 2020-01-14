cbfb09630f2e1f8cd3beb15b0e3ea5dffea49bdd
Arkadiusz Szreiber <szreiber.arkadiusz@gmail.com>
2020-01-14 03:30:51 +0100

    298. Creating & Storing the User Data

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpErrorResponse} from '@angular/common/http';
-import {catchError} from 'rxjs/operators';
-import {throwError} from 'rxjs';
+import {catchError, tap} from 'rxjs/operators';
+import {Subject, throwError} from 'rxjs';
+import {User} from './user.model';
 
 export interface AuthResponseData {
   idToken: string,
@@ -14,6 +15,8 @@ export interface AuthResponseData {
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
+
+  user = new Subject<User>();
 
   constructor(private http: HttpClient) {
   }
@@ -28,7 +31,14 @@ export class AuthService {
           returnSecureToken: true
         }
       )
-      .pipe(catchError(this.handleError));
+      .pipe(catchError(this.handleError),
+        tap(responseData => {
+          this.handleAuthentication(responseData.email,
+            responseData.localId,
+            responseData.idToken,
+            +responseData.expiresIn);
+        })
+      );
   }
 
   login(email: string, password: string) {
@@ -40,7 +50,25 @@ export class AuthService {
           password: password,
           returnSecureToken: true
         }
-      ).pipe(catchError(this.handleError));
+      )
+      .pipe(
+        catchError(this.handleError),
+        tap(responseData => {
+          this.handleAuthentication(responseData.email,
+            responseData.localId,
+            responseData.idToken,
+            +responseData.expiresIn);
+        })
+      );
+  }
+
+  private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
+    const expirationDate = new Date(new Date().getTime() + +expiresIn * 1000);
+    const user = new User(email,
+      userId,
+      token,
+      expirationDate);
+    this.user.next(user);
   }
 
   private handleError(errorResponse: HttpErrorResponse) {
