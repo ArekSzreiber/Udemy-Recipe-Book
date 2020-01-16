b260dace5817be772bfdb036920623ecb8a71180
Arkadiusz Szreiber <szreiber.arkadiusz@gmail.com>
2020-01-16 18:26:19 +0100

    Routes for RecipeModule moved to separate module

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -5,11 +5,8 @@ import {RecipeItemComponent} from './recipe-list/recipe-item/recipe-item.compone
 import {RecipeDetailComponent} from './recipe-detail/recipe-detail.component';
 import {RecipeStartComponent} from './recipe-start/recipe-start.component';
 import {RecipeEditComponent} from './recipe-edit/recipe-edit.component';
-import {RouterModule} from '@angular/router';
 import {CommonModule} from '@angular/common';
 import {ReactiveFormsModule} from '@angular/forms';
-import {AuthGuard} from '../auth/auth.guard';
-import {RecipesResolverService} from './recipes-resolver.service';
 import {RecipesRoutingModule} from './recipes-routing.module';
 
 @NgModule({
@@ -26,8 +23,7 @@ import {RecipesRoutingModule} from './recipes-routing.module';
     ReactiveFormsModule,
     RecipesRoutingModule,
   ],
-  exports: [
-  ]
+  exports: []
 })
 export class RecipesModule {
 
