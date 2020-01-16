9aa85c696031cafdce4edf57fbab31a75eb1bd76
Arkadiusz Szreiber <szreiber.arkadiusz@gmail.com>
2020-01-16 13:41:42 +0100

    312. Preparing Programmatic Creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import {AuthComponent} from './auth/auth.component';
 import {LoadingSpinnerComponent} from './shared/loading-spinner/loading-spinner.component';
 import {AuthInterceptorService} from './auth/auth-interceptor.service';
 import {AlertComponent} from './shared/alert/alert.component';
+import {PlaceholderDirective} from './shared/placeholder/placeholder.directive';
 
 @NgModule({
   declarations: [
@@ -30,6 +31,7 @@ import {AlertComponent} from './shared/alert/alert.component';
     DropdownDirective,
     HeaderComponent,
     LoadingSpinnerComponent,
+    PlaceholderDirective,
     RecipesComponent,
     RecipeListComponent,
     RecipeItemComponent,
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,8 +1,9 @@
-import {Component} from '@angular/core';
+import {Component, ComponentFactoryResolver} from '@angular/core';
 import {NgForm} from '@angular/forms';
 import {AuthResponseData, AuthService} from './auth.service';
 import {Observable} from 'rxjs';
 import {Router} from '@angular/router';
+import {AlertComponent} from '../shared/alert/alert.component';
 
 @Component({
   selector: 'app-auth',
@@ -14,7 +15,8 @@ export class AuthComponent {
   error: string = null;
 
   constructor(private authService: AuthService,
-              private router: Router) {
+              private router: Router,
+              private componentFactoryResolver: ComponentFactoryResolver) {
   }
 
   onSwitchMode() {
@@ -47,6 +49,7 @@ export class AuthComponent {
       errorMessage => {
         console.log(errorMessage);
         this.error = errorMessage;
+        this.showErrorAlert(error);
         this.isLoading = false;
 
       }
@@ -57,5 +60,10 @@ export class AuthComponent {
 
   onHandleError() {
     this.error = null;
+  }
+
+  showErrorAlert() {
+    const alertComponentFactory = this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
+
   }
 }
