ac57b2397297d78dbdf2e260737266ac7d771896
Arkadiusz Szreiber <szreiber.arkadiusz@gmail.com>
2020-01-17 04:34:08 +0100

    327. Understanding the Core Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,13 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {ShoppingListService} from './shopping-list/shopping-list.service';
 import {AppRoutingModule} from './app-routing.module';
-import {RecipeService} from './recipes/recipe.service';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {HttpClientModule} from '@angular/common/http';
 import {AuthComponent} from './auth/auth.component';
-import {AuthInterceptorService} from './auth/auth-interceptor.service';
 import {RecipesModule} from './recipes/recipes.module';
 import {ShoppingListModule} from './shopping-list/shopping-list.module';
 import {SharedModule} from './shared/shared.module';
+import {CoreModule} from './core.module';
 
 @NgModule({
   declarations: [
@@ -22,6 +20,7 @@ import {SharedModule} from './shared/shared.module';
   ],
   imports: [
     AppRoutingModule,
+    CoreModule,
     BrowserModule,
     FormsModule,
     HttpClientModule,
@@ -29,11 +28,6 @@ import {SharedModule} from './shared/shared.module';
     RecipesModule,
     SharedModule,
     ShoppingListModule,
-  ],
-  providers: [
-    ShoppingListService,
-    RecipeService,
-    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true},
   ],
   bootstrap: [AppComponent],
 })
