9d9a2464a3cafc19306c71fd9d5520523259e851
Arkadiusz Szreiber <szreiber.arkadiusz@gmail.com>
2020-01-21 17:04:06 +0100

    363. Auth Finished (For Now...)

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -4,10 +4,14 @@ import {Observable} from 'rxjs';
 import {AuthService} from './auth.service';
 import {map, take} from 'rxjs/operators';
 
+import * as fromApp from '../store/app.reducer';
+import {Store} from '@ngrx/store';
+
 @Injectable({providedIn: 'root'})
 export class AuthGuard implements CanActivate {
 
   constructor(private authService: AuthService,
+              private store: Store<fromApp.AppState>,
               private router: Router) {
   }
 
@@ -15,8 +19,9 @@ export class AuthGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): boolean | UrlTree | Promise<boolean | UrlTree> | Observable<boolean | UrlTree> {
-    return this.authService.user.pipe(
+    return this.store.select('auth').pipe(
       take(1),
+      map(authState => authState.user),
       map(user => {
         const isAuth = !!user;
         if (isAuth) {
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,6 +3,10 @@ import {DataStorageService} from '../shared/data-storage.service';
 import {AuthService} from '../auth/auth.service';
 import {Subscription} from 'rxjs';
 
+import * as fromApp from '../store/app.reducer';
+import {Store} from '@ngrx/store';
+import {map} from 'rxjs/operators';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -14,6 +18,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
   private userSub: Subscription;
 
   constructor(private dataStorageService: DataStorageService,
+              private store: Store<fromApp.AppState>,
               private authService: AuthService) {
   }
 
@@ -26,9 +31,12 @@ export class HeaderComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
-    this.userSub = this.authService.user.subscribe(user => {
-      this.isAuthenticated = !!user;
-    });
+    this.userSub = this.store
+      .select('auth')
+      .pipe(map(authState => authState.user))
+      .subscribe(user => {
+        this.isAuthenticated = !!user;
+      });
   }
 
   ngOnDestroy(): void {
