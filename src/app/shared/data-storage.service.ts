bd8ff7672a8b08a0c6f40dee3074e9ddc5ae77f8
Arkadiusz Szreiber <szreiber.arkadiusz@gmail.com>
2020-01-26 13:25:59 +0100

    379. Getting Started with NgRx for Recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -5,13 +5,20 @@ import {map, tap} from 'rxjs/operators';
 import {Recipe} from '../recipes/recipe.model';
 import {RecipeService} from '../recipes/recipe.service';
 import {AuthService} from '../auth/auth.service';
+import {Store} from '@ngrx/store';
+
+
+import * as fromApp from '../store/app.reducer';
+import * as RecipesActions from '../recipes/store/recipe.actions';
+
+
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
   constructor(
     private http: HttpClient,
     private recipeService: RecipeService,
-    private authService: AuthService
+    private store: Store<fromApp.AppState>,
   ) {
   }
 
@@ -42,7 +49,8 @@ export class DataStorageService {
           });
         }),
         tap(recipes => {
-          this.recipeService.setRecipes(recipes);
+          // this.recipeService.setRecipes(recipes);
+          this.store.dispatch(new  RecipesActions.SetRecipes(recipes));
         })
       );
   }
